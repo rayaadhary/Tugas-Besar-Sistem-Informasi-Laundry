d6b786daff3f6241d55951b4fc854a8e3c0aeeb7
MujahidRamdhani <94102192+MujahidRamdhani@users.noreply.github.com>
2022-07-21 10:48:46 +0700

    Add files via upload

---
+++ konsumen/delete.php
@@ -0,0 +1,15 @@
+<?php
+
+require 'functions.php';
+$conn =dbConnect();
+if($_GET['id_konsumen']!=""){
+    $id = $_GET['id_konsumen'];
+    $sql= mysqli_query($conn,"DELETE FROM konsumen WHERE id_konsumen='$id'");
+    if($sql){
+        $_SESSION['info'] = 'Dihapus';
+        echo "<script>document.location.href='konsumen.php'</script>";
+    }else{
+        $_SESSION['info'] = 'Gagal Dihapus';
+        echo "<script>document.location.href='konsumen.php'</script>";
+    }
+}
